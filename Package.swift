1d70a7cdd36763073ff41881eb004fa3377e8f4f
Yifei Teng <tengyifei@users.noreply.github.com>
2018-03-30 21:54:41 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
       .library(name: "SwiftAWSS3", targets: ["SwiftAWSS3"]),
   ],
   dependencies: [
-      .package(url: "https://github.com/noppoMan/aws-sdk-swift-core.git", .upToNextMajor(from: "1.0.0"))
+      .package(url: "https://github.com/Maaimusic/aws-sdk-swift-core.git", .upToNextMajor(from: "1.0.0"))
   ],
   targets: [
       .target(name: "SwiftAWSS3", dependencies: ["AWSSDKSwiftCore"]),
